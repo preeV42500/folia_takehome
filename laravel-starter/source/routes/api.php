464e01c2447283a42e06744771be1b54b19b2cf1
Peter Virtue <peter.virtue@foliahealth.com>
2024-05-20 16:56:25 -0400

    Adds in simple user based endpoints to serve as an example, as well as adds in some further documentation on getting started with the laravel starter

--- laravel-starter/source/routes/api.php
+++ laravel-starter/source/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Define routes here...
+/**
+ * Use this file to define new API routes under the /api/... path
+ * 
+ * Here are some example, user related endpoints we have established as an example
+ */
 
-Route::get('/test', function () {
-    return 'This is a route.';
-});
+Route::get('/users/{id}', [UserController::class, 'read']);
+Route::post('/users', [UserController::class, 'create']);
